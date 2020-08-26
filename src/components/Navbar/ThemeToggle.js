eaadc0fc72f546ace5302d4f1351a7b4f5fa83f1
Andre Wohnsland <Andre_Wohnsland@web.de>
2020-08-26 22:09:20 +0200

    Improved themeselector

--- src/components/Navbar/ThemeToggle.js
+++ src/components/Navbar/ThemeToggle.js
@@ -21,18 +21,25 @@ class ThemeToggle extends Component {
       this.setState(({ isChecked }) => ({
         isChecked: !isChecked,
       }));
-
-      // cacheTheme(this.state.isChecked);
     };
+
+    const inactiveFont = { color: '#ececec80' };
+    const activeFont = { color: 'rgb(245, 245, 245)' };
+    const darkfont = isLightTheme ? inactiveFont : activeFont;
+    const lightfont = isLightTheme ? activeFont : inactiveFont;
     return (
       <>
         <div className='sliderholder'>
-          <p className='themefont'>Dark Theme:{'\u00a0\u00a0'}</p>
+          <p className='themefont' style={lightfont}>
+            Light{'\u00a0'}
+          </p>
           <label className='switch'>
             <input type='checkbox' checked={!isLightTheme} onChange={toggleCheckboxChange} />
             <span className='slider round'></span>
           </label>
-          <p className='themefont'>{'\u00a0\u00a0'}</p>
+          <p className='themefont' style={darkfont}>
+            {'\u00a0'}Dark {'\u00a0\u00a0\u00a0'}
+          </p>
         </div>
       </>
     );

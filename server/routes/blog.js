aefe9506648937b610274183b3ba41470beaa347
Andre Wohnsland <Andre_Wohnsland@web.de>
2020-08-27 17:47:12 +0200

    Created routes

---
+++ server/routes/blog.js
@@ -0,0 +1,43 @@
+const router = require('express').Router();
+let Blog = require('../models/blog.model');
+
+router.route('/').get((req, res) => {
+  Blog.find()
+    .then((blog) => res.json(blog))
+    .catch((err) => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').get((req, res) => {
+  Blog.findById(req.params.id)
+    .then((blog) => res.json(blog))
+    .catch((err) => res.status(400).json('Error: ' + err));
+});
+
+router.route('/update/:id').post((req, res) => {
+  Blog.findById(req.params.id)
+    .then((blog) => {
+      blog.title = req.body.title;
+      blog.description = req.body.description;
+      blog.text = req.body.blogFile;
+
+      blog
+        .save()
+        .then(() => res.json('Blog updated'))
+        .catch((err) => res.status(400).json('Error: ' + err));
+    })
+    .catch((err) => res.status(400).json('Error: ' + err));
+});
+
+router.route('/add').post((req, res) => {
+  const title = req.body.title;
+  const description = req.body.description;
+  const blogFile = req.body.blogFile;
+
+  const newBlog = Blog({ title, description, blogFile });
+  newBlog
+    .save()
+    .then(() => res.json('Blog added!'))
+    .catch((err) => res.status(400).json('Error: ' + err));
+});
+
+module.exports = router;
---
+++ server/routes/project.js
@@ -0,0 +1,45 @@
+const router = require('express').Router();
+let Project = require('../models/project.model');
+
+router.route('/').get((req, res) => {
+  Project.find()
+    .then((project) => res.json(project))
+    .catch((err) => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').get((req, res) => {
+  Project.findById(req.params.id)
+    .then((project) => res.json(project))
+    .catch((err) => res.status(400).json('Error: ' + err));
+});
+
+router.route('/update/:id').post((req, res) => {
+  Project.findById(req.params.id)
+    .then((project) => {
+      project.title = req.body.title;
+      project.description = req.body.description;
+      project.text = req.body.text;
+      project.link = req.body.link;
+
+      project
+        .save()
+        .then(() => res.json('Project updated'))
+        .catch((err) => res.status(400).json('Error: ' + err));
+    })
+    .catch((err) => res.status(400).json('Error: ' + err));
+});
+
+router.route('/add').post((req, res) => {
+  const title = req.body.title;
+  const description = req.body.description;
+  const text = req.body.text;
+  const link = req.body.link;
+
+  const newProject = Project({ title, description, text, link });
+  newProject
+    .save()
+    .then(() => res.json('Project added!'))
+    .catch((err) => res.status(400).json('Error: ' + err));
+});
+
+module.exports = router;

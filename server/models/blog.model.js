d995c012c56efe31152df43eaf2818dc89b2766d
Andre Wohnsland <Andre_Wohnsland@web.de>
2020-08-27 17:47:01 +0200

    Created models

---
+++ server/models/blog.model.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const blogSchema = new Schema(
+  {
+    title: { type: String, required: true, unique: true },
+    description: { type: String, required: true },
+    blogFile: { type: String, data: Buffer, required: true },
+  },
+  { timestamps: true }
+);
+
+const Blog = mongoose.model('Blog', blogSchema);
+
+module.exports = Blog;

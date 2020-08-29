3e0d26baab9fb6c3856af5b85aee828ce2e0351b
Andre Wohnsland <Andre_Wohnsland@web.de>
2020-08-29 12:42:30 +0200

    Moved reading cache to constructor

--- src/components/Navbar/ThemeToggle.js
+++ src/components/Navbar/ThemeToggle.js
@@ -4,24 +4,24 @@ import './sliderStyle.css';
 
 class ThemeToggle extends Component {
   static contextType = ThemeContext;
-  state = {
-    isChecked: null,
-  };
-
-  componentDidMount() {
+  constructor(props, context) {
+    super(props, context);
     const { getCachedTheme } = this.context;
-    this.setState({ isChecked: getCachedTheme() });
+    this.state = {
+      isChecked: !getCachedTheme(),
+    };
   }
+
+  toggleCheckboxChange = () => {
+    const { setTheme } = this.context;
+    setTheme(this.state.isChecked);
+    this.setState(({ isChecked }) => ({
+      isChecked: !isChecked,
+    }));
+  };
 
   render() {
     const { isLightTheme } = this.context;
-    const toggleCheckboxChange = () => {
-      const { setTheme } = this.context;
-      setTheme(this.state.isChecked);
-      this.setState(({ isChecked }) => ({
-        isChecked: !isChecked,
-      }));
-    };
 
     const inactiveFont = { color: '#ececec80' };
     const activeFont = { color: 'rgb(245, 245, 245)' };
@@ -34,7 +34,7 @@ class ThemeToggle extends Component {
             Light{'\u00a0'}
           </p>
           <label className='switch'>
-            <input type='checkbox' checked={!isLightTheme} onChange={toggleCheckboxChange} />
+            <input type='checkbox' checked={!isLightTheme} onChange={this.toggleCheckboxChange} />
             <span className='slider round'></span>
           </label>
           <p className='themefont' style={darkfont}>

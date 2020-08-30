3160172475d6ec98fa6c9af728925bcbd7282e5f
Andre Wohnsland <Andre_Wohnsland@web.de>
2020-08-30 16:37:20 +0200

    Changed from file to text type

--- server/routes/blog.js
+++ server/routes/blog.js
@@ -18,7 +18,7 @@ router.route('/update/:id').post((req, res) => {
     .then((blog) => {
       blog.title = req.body.title;
       blog.description = req.body.description;
-      blog.text = req.body.blogFile;
+      blog.text = req.body.text;
 
       blog
         .save()
@@ -31,9 +31,9 @@ router.route('/update/:id').post((req, res) => {
 router.route('/add').post((req, res) => {
   const title = req.body.title;
   const description = req.body.description;
-  const blogFile = req.body.blogFile;
+  const text = req.body.text;
 
-  const newBlog = Blog({ title, description, blogFile });
+  const newBlog = Blog({ title, description, text });
   newBlog
     .save()
     .then(() => res.json('Blog added!'))

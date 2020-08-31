31d3ef08d00984a332c8bbac134b7c85ad9d8a18
Andre Wohnsland <Andre_Wohnsland@web.de>
2020-08-31 17:43:19 +0200

    Added text to about

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -10,14 +10,26 @@ const About = () => {
       <div className='main-text'>
         <p>
           This page was created while practising and improving my react knowledge. It is also used to present some of my
-          other coding projects.
+          other coding projects and give me the possibility to write blog entries. Here is a list of the mandatory tools
+          and frameworks used in combination with this side
         </p>
-        <p style={{ textAlign: 'left', paddingLeft: '15%' }}>Tools used are:</p>
-        <ul style={{ listStylePosition: 'inside', textAlign: 'left', paddingLeft: '15%' }}>
+        <p style={{ textAlign: 'left', paddingLeft: '12%', marginBottom: '0px' }}>For the Frontend:</p>
+        <ul style={{ listStylePosition: 'inside', textAlign: 'left', paddingLeft: '12%' }}>
           <li>React</li>
-          <li>React Router</li>
-          <li>React Bootstrap</li>
-          <li>Some Other Frameworks</li>
+          <li>React Router (Dom)</li>
+          <li>React (Router) Bootstrap</li>
+          <li>React Query</li>
+          <li>React Markdown</li>
+          <li>React Syntax Highlighter</li>
+        </ul>
+        <p style={{ textAlign: 'left', paddingLeft: '12%', marginBottom: '0px' }}>For the Backend:</p>
+        <ul style={{ listStylePosition: 'inside', textAlign: 'left', paddingLeft: '12%' }}>
+          <li>Express</li>
+          <li>MongoDB / Mongoose</li>
+          <li>CORS</li>
+          <li>Jsonwebtoken</li>
+          <li>Cookie Parser</li>
+          <li>Brypt</li>
         </ul>
       </div>
     </div>

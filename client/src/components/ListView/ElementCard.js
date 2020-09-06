e0e483660a8a4d0bac6113adc5b056fe9ea1fe7f
Andre Wohnsland <Andre_Wohnsland@web.de>
2020-09-06 15:06:59 +0200

    Added info for create und update time to card

--- client/src/components/ListView/ElementCard.js
+++ client/src/components/ListView/ElementCard.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import dateFormatter from './dateFormatter';
 
 const ElementCard = ({ element, elementType }) => {
+  const createdAt = dateFormatter(element.createdAt);
+  const updatedAt = dateFormatter(element.updatedAt);
+
   return (
     <div key={element._id}>
       <Link to={`/${elementType}/${element._id}`} style={{ textDecoration: 'none' }}>
         <div className='card-div'>
           <div className='card-content'>
             <h3 className='card-title'>{element.title}</h3>
+            <span className='card-info'>
+              {createdAt === updatedAt ? (
+                <span>Created {dateFormatter(element.createdAt)} </span>
+              ) : (
+                <span> Updated {dateFormatter(element.updatedAt)}</span>
+              )}
+            </span>
             <p className='card-desc'>{element.description}</p>
           </div>
         </div>

c96a8a5801ae21cd6f807ca2ba5024d146a039ae
Andre Wohnsland <Andre_Wohnsland@web.de>
2020-09-10 21:13:26 +0200

    Added init of admin user if no user exists

--- setUp/initMongodb.js
+++ setUp/initMongodb.js
@@ -1,8 +1,16 @@
 const mongoose = require('mongoose');
+const User = require('../models/user.model');
 
 function initMongodb() {
   const uri = process.env.ATLAS_URI;
-  mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+  mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }).then(async () => {
+    const existingUser = await User.find();
+    if (existingUser.length === 0) {
+      console.log('No user exists, generating default user ...');
+      const newUser = User({ username: 'admin', password: 'admin' });
+      await newUser.save().then(console.los('User admin was generated. Please keep in mind changing creds.'));
+    }
+  });
   const connection = mongoose.connection;
   connection.once('open', () => {
     console.log('MongoDB database connection established successfully');

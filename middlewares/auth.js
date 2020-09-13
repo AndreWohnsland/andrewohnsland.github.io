b65ce2c55587437e0089a7952bd3c7adfd6b0d9a
Andre Wohnsland <Andre_Wohnsland@web.de>
2020-09-13 13:55:08 +0200

    Added validation of the cookie data

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,10 +1,16 @@
 const jwt = require('jsonwebtoken');
 const { AppError } = require('./errorHandler');
+const User = require('../models/user.model');
 
-module.exports = (req, res, next) => {
+module.exports = async (req, res, next) => {
   try {
     const token = req.cookies['jwt'];
-    jwt.verify(token, process.env.JWT_SECRET);
+    const tokenDetails = jwt.verify(token, process.env.JWT_SECRET);
+    const foundUser = await User.findById(tokenDetails.id);
+    if (foundUser === null) {
+      res.cookie('jwt', '', { httpOnly: true, maxAge: 0, sameSite: 'lax' });
+      return next(new AppError('Invalid token data', 401));
+    }
     next();
   } catch (error) {
     next(new AppError('Authentication failed', 401));

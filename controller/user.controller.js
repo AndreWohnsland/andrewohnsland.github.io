d9d88982e9a287187c0ad68b3ed02eb81fa18768
Andre Wohnsland <Andre_Wohnsland@web.de>
2020-09-13 14:07:31 +0200

    Added api for password change

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -3,8 +3,7 @@ const jwt = require('jsonwebtoken');
 const { AppError } = require('../middlewares/errorHandler');
 
 async function addUser(req, res, next) {
-  const username = req.body.username;
-  const password = req.body.password;
+  const { username, password } = req.body;
 
   const newUser = User({ username, password });
   newUser
@@ -13,9 +12,21 @@ async function addUser(req, res, next) {
     .catch((err) => next(new AppError('Error: ' + err, 400)));
 }
 
+async function changePassword(req, res, next) {
+  const { username, password, newPassword } = req.body;
+  if (newPassword.length < 8) {
+    return next(new AppError('Password needs to have at least 8 letters', 400));
+  }
+  User.login(username, password)
+    .then((user) => {
+      user.password = newPassword;
+      user.save().then(res.status(200).json('Password changed successfully'));
+    })
+    .catch((err) => next(new AppError(`${err}`, 400)));
+}
+
 async function login(req, res, next) {
-  const username = req.body.username;
-  const password = req.body.password;
+  const { username, password } = req.body;
 
   User.login(username, password)
     .then((user) => {
@@ -37,4 +48,4 @@ const createToken = (id) => {
   });
 };
 
-module.exports = { addUser, login, getAuth };
+module.exports = { addUser, login, getAuth, changePassword };
--- routes/user.js
+++ routes/user.js
@@ -5,6 +5,7 @@ const userController = require('../controller/user.controller');
 router.route('/add').post(authorize, userController.addUser);
 
 router.route('/login').post(userController.login);
+router.route('/change').post(authorize, userController.changePassword);
 
 router.route('/auth').get(authorize, userController.getAuth);
 

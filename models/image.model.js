cf63805a0066d2e12fb55e214e124a6a4417ec37
Andre Wohnsland <Andre_Wohnsland@web.de>
2020-10-19 22:33:21 +0200

    All values are required now

--- models/image.model.js
+++ models/image.model.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 const imageSchema = new mongoose.Schema(
   {
-    name: String,
-    description: String,
+    name: { type: String, required: [true, 'Please enter a name'] },
+    description: { type: String, required: [true, 'Please enter a description'] },
     img: {
       data: Buffer,
       contentType: String,

9599ec15ce24817f857a7f99f9c10b979c29534c
Andre Wohnsland <Andre_Wohnsland@web.de>
2020-10-19 22:27:06 +0200

    Removed unused parts

--- routes/image.js
+++ routes/image.js
@@ -1,10 +1,8 @@
 const router = require('express').Router();
 const authorize = require('../middlewares/auth');
 const imageController = require('../controller/image.controller');
-const upload = require('../middlewares/upload');
 
 router.route('/').get(imageController.getImages);
 router.route('/add').post(authorize, imageController.addImage);
-// router.route('/add').post(authorize, upload.single('image'), imageController.addImage);
 
 module.exports = router;

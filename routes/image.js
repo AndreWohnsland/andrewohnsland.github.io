c3b845e0e61f8f13e233d01ac3263502cd929a45
Andre Wohnsland <Andre_Wohnsland@web.de>
2020-10-21 17:43:09 +0200

    Added delete and all route

--- routes/image.js
+++ routes/image.js
@@ -2,8 +2,13 @@ const router = require('express').Router();
 const authorize = require('../middlewares/auth');
 const imageController = require('../controller/image.controller');
 
+// get
 router.route('/fotography').get(imageController.getFotographs);
 router.route('/woodwork').get(imageController.getWoodworks);
+router.route('/all/details').get(imageController.getAllDetails);
 router.route('/add').post(authorize, imageController.addImage);
+
+// delete
+router.route('/delete/:id').delete(authorize, imageController.deleteImage);
 
 module.exports = router;

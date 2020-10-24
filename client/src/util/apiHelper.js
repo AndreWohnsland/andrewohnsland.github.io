0eeae5010297c38487af3b2e3c18191179a24f7b
Andre Wohnsland <Andre_Wohnsland@web.de>
2020-10-24 09:55:18 +0200

    Moved api calls to helper

--- client/src/util/apiHelper.js
+++ client/src/util/apiHelper.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
+import arrayBufferToBase64 from './binaryConverter';
 
 const api_address = process.env.REACT_APP_API_ADDRESS;
+const api = `${api_address}/api`;
 
 const getAndGenerateImageDetails = async () => {
-  const res = await axios.get(`${api_address}/api/image/all/details`);
+  const res = await axios.get(`${api}/image/all/details`);
   const start = [{ name: 'Select image', value: 'noId' }];
   const imgList = res.data.map((img) => {
     const suff = img.category !== undefined ? ` (${img.category})` : '';
@@ -15,8 +17,25 @@ const getAndGenerateImageDetails = async () => {
   return [...start, ...imgList];
 };
 
+const getAllImageData = async (pictype) => {
+  const { data } = await axios.get(`${api}/image/${pictype}`);
+  const returnData = await data.map((obj) => {
+    return {
+      width: obj.width,
+      height: obj.height,
+      src: 'data:image/jpeg;base64,' + arrayBufferToBase64(obj.img.data.data),
+      title: obj.name,
+    };
+  });
+  return returnData;
+};
+
+const postImage = (data) => {
+  return axios.post(`${api}/image/add`, data, { withCredentials: true, validateStatus: () => true });
+};
+
 const deleteImage = (imageId) => {
-  return axios.delete(`${api_address}/api/image/delete/${imageId}`, {
+  return axios.delete(`${api}/image/delete/${imageId}`, {
     withCredentials: true,
     validateStatus: () => true,
   });
@@ -24,7 +43,7 @@ const deleteImage = (imageId) => {
 
 const loginUser = (username, password) => {
   return axios.post(
-    `${api_address}/api/user/login`,
+    `${api}/user/login`,
     { username, password },
     {
       withCredentials: true,
@@ -34,4 +53,65 @@ const loginUser = (username, password) => {
   );
 };
 
-export { getAndGenerateImageDetails, deleteImage, loginUser };
+const getElementData = async (elementType, id) => {
+  const { data } = await axios.get(`${api}/${elementType}/${id}`);
+  return data;
+};
+
+const getElements = async (elementType) => {
+  const { data } = await axios.get(`${api}/${elementType}`);
+  return data;
+};
+
+const addElement = (dataToSend, elementType) => {
+  return axios.post(`${api}/${elementType}/add`, dataToSend, {
+    withCredentials: true,
+    validateStatus: () => true,
+  });
+};
+
+const updateElement = (dataToSend, elementType, elementId) => {
+  return axios.post(`${api}/${elementType}/update/${elementId}`, dataToSend, {
+    withCredentials: true,
+    validateStatus: () => true,
+  });
+};
+
+const updatePassword = (username, password, newPassword, repeatedPassword) => {
+  return axios.post(
+    `${api}/user/change`,
+    { username, password, newPassword, repeatedPassword },
+    {
+      withCredentials: true,
+      headers: { 'Content-Type': 'application/json' },
+      validateStatus: () => true,
+    }
+  );
+};
+
+const getAuth = () => {
+  return axios
+    .get('http://localhost:5000/api/user/auth', {
+      withCredentials: true,
+    })
+    .then(() => {
+      return true;
+    })
+    .catch(() => {
+      return false;
+    });
+};
+
+export {
+  getAndGenerateImageDetails,
+  deleteImage,
+  getAllImageData,
+  loginUser,
+  getElementData,
+  getElements,
+  addElement,
+  updateElement,
+  updatePassword,
+  postImage,
+  getAuth,
+};

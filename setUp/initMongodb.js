0a82f3b3e37dc9ce3892bad7b5b7c3c2759b303d
Andre Wohnsland <Andre_Wohnsland@web.de>
2020-11-02 19:55:48 +0100

    Seperated initial user info

--- setUp/initMongodb.js
+++ setUp/initMongodb.js
@@ -3,6 +3,8 @@ const pino = require('pino');
 const User = require('../models/user.model');
 
 const logger = pino({ level: process.env.LOG_LEVEL || 'info', prettyPrint: true });
+const username = 'admin';
+const password = 'adminadmin';
 
 function initMongodb() {
   const uri = process.env.ATLAS_URI;
@@ -10,12 +12,10 @@ function initMongodb() {
     const existingUser = await User.find();
     if (existingUser.length === 0) {
       logger.info('No user exists, generating default user ...');
-      const newUser = User({ username: 'admin', password: 'adminadmin' });
-      await newUser
-        .save()
-        .then(
-          logger.info('User "admin" with password "adminadmin" was generated. Please keep in mind changing creds.')
-        );
+      const newUser = User({ username, password });
+      await newUser.save().then(() => {
+        logger.info('User "admin" with password "adminadmin" was generated. Please keep in mind changing creds.');
+      });
     }
   });
   const { connection } = mongoose;

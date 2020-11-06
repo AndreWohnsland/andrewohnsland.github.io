287ba86214d0c5dd7a1a8ef9b83dbdf4f1e4ce5e
Andre Wohnsland <Andre_Wohnsland@web.de>
2020-11-06 19:05:39 +0100

    Added logging for cors

--- setUp/initCors.js
+++ setUp/initCors.js
@@ -1,4 +1,7 @@
 const cors = require('cors');
+const pino = require('pino');
+
+const logger = pino({ level: process.env.LOG_LEVEL || 'info', prettyPrint: true });
 
 function initCors(app) {
   const whitelist = ['http://localhost:3000', 'https://andrewohnsland.github.io'];
@@ -7,6 +10,7 @@ function initCors(app) {
       if (whitelist.indexOf(origin) !== -1) {
         callback(null, true);
       } else {
+        logger.warn(`Not authorized acces from: ${origin}`);
         callback(new Error(`${origin}: Not allowed by CORS`));
       }
     },

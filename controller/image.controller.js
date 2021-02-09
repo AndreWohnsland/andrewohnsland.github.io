f4e87fc485c92fe2920f5168d9a9bc7a4d2b8dc5
Andre Wohnsland <Andre_Wohnsland@web.de>
2021-02-09 17:13:22 +0100

    Refactored code to utils, added dropbox pic saving

--- controller/image.controller.js
+++ controller/image.controller.js
@@ -1,36 +1,42 @@
 const sizeOf = require('buffer-image-size');
-const sharp = require('sharp');
 const pino = require('pino');
-const Image = require('../models/image.model');
+const Image = require('../models/imageDB.model');
 const { AppError } = require('../middlewares/errorHandler');
+const {
+  resizeImage,
+  bufferToStream,
+  postPictureToDropbox,
+  generateShareableLink,
+  deletePictureFromDropbox,
+} = require('./imageUtils');
 
 const logger = pino({ level: process.env.LOG_LEVEL || 'info', prettyPrint: true });
 
-const SIZE_LONG_SIDE = 800;
 const cat = { foto: 'fotography', wood: 'woodwork' };
-
-async function resizeImage(buffer) {
-  const dimensions = sizeOf(buffer);
-  const { height, width } = dimensions;
-  let resizeOptions = { height: SIZE_LONG_SIDE };
-  if (width > height) resizeOptions = { width: SIZE_LONG_SIDE };
-  const ret = await sharp(buffer).resize(resizeOptions).toBuffer();
-  return ret;
-}
 
 async function addImage(req, res, next) {
   const { name, category } = req.body;
   let receivedFile = req.files;
   if (receivedFile === undefined || receivedFile === null) return next(new AppError('No file given', 400));
   receivedFile = receivedFile.file;
+
+  const sameName = await Image.findOne({ name });
+  if (sameName !== null) return next(new AppError('Name already exists', 400));
+
   const resizedFile = await resizeImage(receivedFile.data);
   const dimensions = sizeOf(resizedFile);
   const { height, width } = dimensions;
-  const img = {
-    data: resizedFile,
-    contentType: receivedFile.mimetype,
-  };
-  const newImage = Image({ name, height, width, img, category });
+
+  const formattedName = name.replace(/\s/g, '');
+  const dropboxPath = `/${category}/${formattedName}.jpg`;
+  const fileStream = bufferToStream(resizedFile);
+
+  await postPictureToDropbox(dropboxPath, fileStream);
+  const sharedLinkResponse = await generateShareableLink(dropboxPath);
+  let sharedUrl = sharedLinkResponse.url;
+  sharedUrl = sharedUrl.replace(/\?dl=0/, '?raw=1');
+
+  const newImage = Image({ name, height, width, img: sharedUrl, category });
   newImage
     .save()
     .then(() => res.json('Picture added'))
@@ -69,6 +75,9 @@ async function deleteImage(req, res, next) {
     .then((img) => {
       if (img) {
         const msg = `Image with name: ${img.name} was deleted.`;
+        const dropboxName = img.name.replace(/\s/g, '');
+        const dropboxPath = `/${img.category}/${dropboxName}.jpg`;
+        deletePictureFromDropbox(dropboxPath);
         logger.info(msg);
         res.json(msg);
       } else {

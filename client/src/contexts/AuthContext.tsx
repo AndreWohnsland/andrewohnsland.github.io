d31d17a48e49085d96d8c89b16ec2143278628c3
Andre Wohnsland <Andre_Wohnsland@web.de>
2021-03-15 17:04:00 +0100

    Converted AuthContex to tsx

---
+++ client/src/contexts/AuthContext.tsx
@@ -0,0 +1,40 @@
+import React, { createContext, useState, useEffect } from 'react';
+import { getAuth } from '../util/apiHelper';
+
+type ContextType = {
+  isAuth: boolean | null;
+  setIsAuth: (state: boolean) => void;
+  getAuthStatus: () => Promise<void>;
+};
+
+export const AuthContext = createContext<ContextType>({} as ContextType);
+
+export const AuthContextProvider: React.FC = ({ children }) => {
+  const [auth, setAuth] = useState<boolean | null>(null);
+
+  const getAuthStatus = async (): Promise<void> => {
+    return getAuth().then((retauth) => {
+      setAuth(retauth);
+    });
+  };
+
+  useEffect(() => {
+    getAuthStatus();
+  }, []);
+
+  const setIsAuth = (state: boolean): void => {
+    setAuth(state);
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isAuth: auth,
+        setIsAuth,
+        getAuthStatus,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};

c7082058ece77c28acc0c494dc9f824aedddede4
Andre Wohnsland <Andre_Wohnsland@web.de>
2021-03-17 17:32:02 +0100

    Added Interfaces for client

---
+++ client/src/Interfaces/image.interface.ts
@@ -0,0 +1,13 @@
+export interface IImageElement {
+  _id: string;
+  width: number;
+  height: number;
+  img: string;
+  name: string;
+  category: string;
+}
+
+export interface IImageReducedDetails {
+  name: string;
+  value: string;
+}

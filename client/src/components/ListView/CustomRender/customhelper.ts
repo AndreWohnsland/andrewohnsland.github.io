8f65006cc86f337578a585ee6a1b4f2c2ee32396
Andre Wohnsland <Andre_Wohnsland@web.de>
2021-03-21 10:54:37 +0100

    Fixed spelling error

--- client/src/components/ListView/CustomRender/customhelper.ts
+++ client/src/components/ListView/CustomRender/customhelper.ts
@@ -1,5 +1,5 @@
 const checkCustomType = (identifier: string | undefined): boolean => {
-  const allowed: string[] = ['warning', 'danger', 'info', 'helpfull'];
+  const allowed: string[] = ['warning', 'danger', 'info', 'helpful'];
   if (identifier === undefined) return false;
   return allowed.includes(identifier);
 };

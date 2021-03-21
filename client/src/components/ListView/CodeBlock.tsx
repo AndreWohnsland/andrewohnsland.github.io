555341a57406fb95844c95c0c2392ef03b7ac400
Andre Wohnsland <Andre_Wohnsland@web.de>
2021-03-21 10:47:11 +0100

    Added custom elements to code blocks

--- client/src/components/ListView/CodeBlock.tsx
+++ client/src/components/ListView/CodeBlock.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { vscDarkPlus } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import CustomRender from './CustomRender/CustomRender';
+import checkCustomType from './CustomRender/customhelper';
 
 type CodeBlockProps = {
   language: string | undefined;
-  value: any;
+  value: string;
 };
 
 const CodeBlock: React.FC<CodeBlockProps> = ({ language, value }) => {
+  // This is currently a hack to also use codeblock for custom elements
+  // I'd love to use shortcodes, but they dont seem to work here
+  if (checkCustomType(language)) {
+    return <CustomRender identifier={language} text={value} />;
+  }
   return (
     <>
       <p className="header-code">{language}</p>

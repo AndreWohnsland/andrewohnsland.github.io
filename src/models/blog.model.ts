46489cbc7d20644f4bbcdf386f8608fc0fa3515b
Andre Wohnsland <Andre_Wohnsland@web.de>
2021-03-23 18:58:55 +0100

    Added category to model

--- src/models/blog.model.ts
+++ src/models/blog.model.ts
@@ -10,6 +10,7 @@ const blogSchema = new Schema(
     title: { type: String, required: [true, 'Please enter a titel'], unique: [true, 'No duplicate title allowed'] },
     description: { type: String, required: [true, 'Please enter a description'] },
     text: { type: String, required: [true, 'Please enter a text'] },
+    category: { type: [String], default: [] },
     draft: { type: Boolean, default: false },
   },
   { timestamps: true },
--- src/models/project.model.ts
+++ src/models/project.model.ts
@@ -10,6 +10,7 @@ const projectSchema = new Schema(
     title: { type: String, required: [true, 'Please enter a titel'], unique: [true, 'No duplicate title allowed'] },
     description: { type: String, required: [true, 'Please enter a description'] },
     text: { type: String, required: [true, 'Please enter a text'] },
+    category: { type: [String], default: [] },
     link: { type: String, required: [true, 'Please enter a link'] },
     draft: { type: Boolean, default: false },
   },

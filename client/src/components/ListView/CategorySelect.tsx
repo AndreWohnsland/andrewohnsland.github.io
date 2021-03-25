5ba2de1acecfc7f0f603cd45529a94e4de286e50
Andre Wohnsland <Andre_Wohnsland@web.de>
2021-03-25 15:46:29 +0100

    Added multi select component

---
+++ client/src/components/ListView/CategorySelect.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import Select from 'react-select';
+import {
+  SelectProps,
+  OnSelectChangeValue,
+  OnSelectChangeActionMeta,
+} from '../../Interfaces/categorySelect.interface';
+
+type CategorySelectProps = {
+  categoryInfo: SelectProps[];
+  categoryValue: SelectProps[];
+  onChange: (e: OnSelectChangeValue, a?: OnSelectChangeActionMeta) => void;
+};
+
+const CategorySelect: React.FC<CategorySelectProps> = ({
+  categoryInfo,
+  categoryValue,
+  onChange,
+}) => {
+  return (
+    <Select
+      isMulti
+      closeMenuOnSelect={false}
+      name="categories"
+      options={categoryInfo}
+      className="basic-multi-select category-multi-select"
+      classNamePrefix="select"
+      placeholder="Select Category to Filter"
+      value={categoryValue}
+      onChange={onChange}
+    />
+  );
+};
+
+export default CategorySelect;

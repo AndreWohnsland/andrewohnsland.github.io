f7faa699c7323fc07cc925fd36f16f7eddf662a0
Andre Wohnsland <Andre_Wohnsland@web.de>
2022-09-22 22:36:14 +0200

    Restructuring of base index

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
+import { createRoot } from 'react-dom/client';
+import './styles/index.scss';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { ThemeContextProvider } from './contexts/ThemeContext';
 import { AuthContextProvider } from './contexts/AuthContext';
 
-ReactDOM.render(
+const root = createRoot(document.getElementById('root') as HTMLElement);
+root.render(
   <React.StrictMode>
     <ThemeContextProvider>
       <AuthContextProvider>
         <App />
       </AuthContextProvider>
     </ThemeContextProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );

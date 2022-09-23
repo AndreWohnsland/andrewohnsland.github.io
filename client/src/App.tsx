4b455932e25007aca5b95b2fcf60983d0cd94da4
Andre Wohnsland <Andre_Wohnsland@web.de>
2022-09-23 16:31:39 +0200

    Adjusted app for routing and r18

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,37 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import NavBar from './components/Navbar/Navbar';
 import Footer from './components/Footer';
 import { ThemeContext } from './contexts/ThemeContext';
 import SwitchComponent from './components/SwitchComponent';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
-class App extends Component {
-  // eslint-disable-next-line react/static-property-placement
-  static contextType = ThemeContext;
+const App: React.FC = () => {
+  const { isLightTheme } = useContext(ThemeContext);
+  const theme = isLightTheme ? 'light' : 'dark';
+  const queryClient = new QueryClient();
 
-  render(): JSX.Element {
-    const { isLightTheme, light, dark } = this.context;
-    const theme = isLightTheme ? light : dark;
-    const style = { backgroundColor: theme.bg, color: theme.syntax };
-    return (
-      <>
+  return (
+    <>
+      <QueryClientProvider client={queryClient}>
         <BrowserRouter>
           <div className="App">
-            <div className="content-container" style={style}>
-              <NavBar />
+            <NavBar />
+            <div className={`content-container container-${theme}`}>
               <div className="main">
                 <SwitchComponent />
               </div>
             </div>
             <Footer />
           </div>
+          {process.env.REACT_APP_DEV === 'true' && (
+            <ReactQueryDevtools initialIsOpen={false} />
+          )}
         </BrowserRouter>
-      </>
-    );
-  }
-}
+      </QueryClientProvider>
+    </>
+  );
+};
 
 export default App;

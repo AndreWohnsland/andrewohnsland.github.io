2a2d94e45f2830391e65ba7d90aa36b96041ee3e
Andre Wohnsland <Andre_Wohnsland@web.de>
2022-10-03 17:33:15 +0200

    Added resource model / schema

---
+++ src/models/resource.model.ts
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+import { IResourceModel } from '../interfaces/resource.interface';
+
+const { Schema } = mongoose;
+
+const resourceSchema = new Schema<IResourceModel>(
+  {
+    name: { type: String, required: [true, 'Please enter a titel'] },
+    filename: { type: String, required: [true, 'Please enter a titel'] },
+    blogId: { type: String, required: [true, 'Please reference a blog'] },
+    link: { type: String, required: [true, 'Please enter a link'] },
+  },
+  { timestamps: true },
+);
+
+const Resource = mongoose.model<IResourceModel>('Resource', resourceSchema);
+
+export default Resource;

afd9c3312535c1f747e8bb9d6a395bb34fc40048
Andre Wohnsland <Andre_Wohnsland@web.de>
2022-10-04 17:47:21 +0200

    Added deletion by blogId

--- src/controller/resource.controller.ts
+++ src/controller/resource.controller.ts
@@ -9,10 +9,14 @@ import logger from '../setUp/initLogger';
 async function addResource(req: Request, res: Response, next: NextFunction) {
   const { name, blogId } = req.body;
   const reqFile = req.files;
-  if (name === undefined || blogId === undefined) return res.status(400).json({ message: 'Missing Data' });
-  if (reqFile === undefined || reqFile === null) return next(new AppError('No file given', 400));
+  if (name === undefined || blogId === undefined || blogId === '')
+    return res.status(400).json({ message: 'Missing Data' });
+  if (reqFile === undefined || reqFile === null) return res.status(400).json({ message: 'No file given' });
   const receivedFile = reqFile.file as UploadedFile;
   const filename = receivedFile.name;
+
+  const douplicate = await Resource.findOne({ filename, blogId });
+  if (douplicate !== null) return res.status(409).json({ message: 'File already exists under this blog' });
 
   const dropboxPath = `/resources/${blogId}/${filename}`;
   const fileStream = bufferToStream(receivedFile.data as Buffer);
@@ -49,10 +53,17 @@ async function deleteResource(req: Request, res: Response, next: NextFunction) {
 }
 
 async function getAllDetails(req: Request, res: Response, next: NextFunction) {
-  Resource.find({}, { name: 1, link: 1 })
+  Resource.find({})
     .sort({ createdAt: -1 })
-    .then((img: IResourceModel[]) => res.json(img))
+    .then((r: IResourceModel[]) => res.json(r))
     .catch((err: Error) => next(new AppError(`Error getting resources ${err}`, 400)));
 }
 
-export default { addResource, deleteResource, getAllDetails };
+async function deleteResourcesUsingBlogId(blogId: string) {
+  await Resource.deleteMany({ blogId });
+  const dropboxPath = `/resources/${blogId}`;
+  await deletePictureFromDropbox(dropboxPath);
+  logger.info(`Deleting all resources from blog with id: ${blogId}`);
+}
+
+export default { addResource, deleteResource, getAllDetails, deleteResourcesUsingBlogId };
